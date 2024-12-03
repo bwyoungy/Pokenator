fea7f0f31ac78b6deeebd777f4b6bdc104e2a308
bwyoungy <youngyaacov@gmail.com>
2024-12-03 11:05:57 +0200

    link to the API used

--- frontend/src/Components/LayoutArea/Footer/Footer.tsx
+++ frontend/src/Components/LayoutArea/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer(): JSX.Element {
     return (
         <div className="Footer">
-			<p>Developed by Ya'acov Young, 2024. All data pulled from PokéAPI</p>
+			<p>Developed by Ya'acov Young, 2024. All data pulled from <a href="https://pokeapi.co/" target="_blank" rel="noopener noreferrer">PokéAPI</a></p>
         </div>
     );
 }
